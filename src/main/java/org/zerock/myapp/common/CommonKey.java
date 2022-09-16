479036b8cc2a732ce27115e8c09b0db5f65b156f
bobae <childhop90@gmail.com>
2022-09-16 22:59:02 +0900

    common 공용 키

---
+++ src/main/java/org/zerock/myapp/common/CommonKey.java
@@ -0,0 +1,9 @@
+package org.zerock.myapp.common;
+
+//공용키
+public class CommonKey {
+
+	public static final String BOARD_RESULT = "BOARD_RESULT";
+	public static final String BOARD_LIST = "BOARD_LIST";
+	public static final String PAGINATION = "PAGINATION";
+}//end class
